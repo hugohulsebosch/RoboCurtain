5c7705e8208f0a1bca740af09ee9461a2d88efac
Hugo Hulsebosch <hugohulsebosch@icloud.com>
2026-04-17 18:44:06 +0200

    Main and 3:00 AM activation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 from alarm import get_wake_up_time
 from logger import get_logger
+from scheduler import schedule_open, schedule_close
+from sun import get_sunrise_and_sunset
+from datetime import timedelta
 
 
 log = get_logger(__name__)
 
-if __name__ == "__main__":
+
+def main():
     log.info("RoboCurtain starting...")
     wake_up = get_wake_up_time()
+    sunrise, sunset = get_sunrise_and_sunset()
     if wake_up:
-        log.info(f"Parsed datetime: {wake_up}")
-    
-    log.info("RoboCurtain is done and also going to sleep. ZzZz...")
+        open_alarm = wake_up - timedelta(minutes=10)
+        open_sun = sunrise + timedelta(minutes=15)
+        schedule_open(max(open_alarm, open_sun))
+
+    schedule_close(sunset + timedelta(minutes=15))
+
+    log.info("RoboCurtain is done scheduling and going to sleep. ZzZz...")
+
+
+if __name__ == "__main__":
+    main()
